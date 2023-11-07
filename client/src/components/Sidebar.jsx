c8c14857761fd004e9c0bfe42c43536b94e7a904
Igor <80968651+webdevbiv@users.noreply.github.com>
2023-11-07 10:12:09 +0200

    ADD | sidebar done

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,7 +4,28 @@ import { sun, logo } from "../assets";
 import { navlinks } from "../constants";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div></div>
+  <div
+    className={`w-[48px] h-[48px] rounded-[10px] ${
+      isActive && isActive === name && "bg-[#2c2f32]"
+    } flex justify-center items-center ${
+      !disabled && "cursor-pointer"
+    } ${styles}`}
+    onClick={handleClick}
+  >
+    {!isActive ? (
+      <img
+        src={imgUrl}
+        alt='fund_logo'
+        className='w-1/2 h-1/2'
+      />
+    ) : (
+      <img
+        src={imgUrl}
+        alt='fund_logo'
+        className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+      />
+    )}
+  </div>
 );
 
 function Sidebar() {
@@ -18,6 +39,28 @@ function Sidebar() {
           imgUrl={logo}
         />
       </Link>
+      <div className='flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12'>
+        <div className='flex flex-col justify-center items-center gap-3'>
+          {navlinks.map((link) => (
+            <Icon
+              key={link.name}
+              {...link}
+              isActive={isActive}
+              handleClick={() => {
+                if (!link.disabled) {
+                  setIsActive(link.name);
+                  navigate(link.link);
+                }
+              }}
+            />
+          ))}
+        </div>
+        <Icon
+          styles={`bg-[#1c1c24] shadow-secondary`}
+          name='sun'
+          imgUrl={sun}
+        />
+      </div>
     </div>
   );
 }
